ae84c256a5b5eb26a80264f99c6cdf4446725a66
davidefranzolivelli <franzinidavide537@gmail.com>
2024-05-06 10:29:35 +0200

    add

--- endpoints/esperienza.js
+++ endpoints/esperienza.js
@@ -1,8 +1,8 @@
 function endpoint(app, connpool) {
 
-    app.post("/api/tasks", (req, res) => {
+    app.post("/api/esperienze", (req, res) => {
         var errors = []
-        /* controllo dati inseriti
+        /* controllo dati inserit
         if (!req.body.description) {
             errors.push("No description specified");
         }
@@ -19,7 +19,7 @@ function endpoint(app, connpool) {
             status: req.body.status,
         }
 
-        var sql = 'INSERT INTO task (description, status) VALUES (?,?)'
+        var sql = 'INSERT INTO esperienza (testo) VALUES (?)'
         var params = [data.description, data.status]
         connpool.query(sql, params, (error, results) => {
             if (error) {
@@ -38,8 +38,8 @@ function endpoint(app, connpool) {
 
 
 
-    app.get("/api/tasks", (req, res, next) => {
-        var sql = "select * from task"
+    app.get("/api/esperienze", (req, res, next) => {
+        var sql = "select * from esperienza"
         var params = []
         connpool.query(sql, params, (err, rows) => {
             if (err) {
@@ -54,8 +54,8 @@ function endpoint(app, connpool) {
     });
 
 
-    app.get("/api/tasks/:id", (req, res) => {
-        var sql = "select * from task where task_id = ?"
+    app.get("/api/esperienze/:id", (req, res) => {
+        var sql = "select * from esperienza where IDesperienza = ?"
         var params = [req.params.id]
         connpool.query(sql, params, (err, rows) => {
             if (err) {
@@ -70,17 +70,15 @@ function endpoint(app, connpool) {
     });
 
 
-    app.put("/api/tasks/:id", (req, res) => {
+    app.put("/api/esperienze/:id", (req, res) => {
         var data = {
-            description: req.body.description,
-            status: req.body.status,
+            testo: req.body.testo
         }
         connpool.execute(
-            `UPDATE task set 
-               description = COALESCE(?,description), 
-               status = COALESCE(?,status) 
-               WHERE task_id = ?`,
-            [data.description, data.status, req.params.id],
+            `UPDATE esperienza set 
+               testo = COALESCE(?), 
+               WHERE IDesperienza = ?`,
+            [data.testo, req.params.id],
             function (err, result) {
                 if (err){
                     res.status(400).json({"error": err.message})
@@ -97,9 +95,9 @@ function endpoint(app, connpool) {
 
 
 
-    app.delete("/api/tasks/:id", (req, res) => {
+    app.delete("/api/esperienze/:id", (req, res) => {
         connpool.execute(
-            'DELETE FROM task WHERE task_id = ?',
+            'DELETE FROM esperienza WHERE IDesperienza = ?',
             [req.params.id],
             function (err, result) {
                 if (err){
