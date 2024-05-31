0cb5992878c478062b5005eb0caf2d97e9137d47
davidefranzolivelli <franzinidavide537@gmail.com>
2024-05-31 10:51:20 +0200

    add

--- endpoints/esperienza.js
+++ endpoints/esperienza.js
@@ -74,7 +74,7 @@ function endpoint(app, connpool) {
             testo: req.body.testo
         }
         connpool.execute(
-            `UPDATE esperienza set testo = ? WHERE IDesperienza = ?`,
+            `UPDATE esperienza SET testo = COALESCE(testo,?) WHERE IDesperienza = ?`,
             [data.testo, req.params.id],
             function (err, result) {
                 if (err){
